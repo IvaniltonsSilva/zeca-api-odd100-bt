15380d73413b2da9fe8403b4536cf7b0505c3fc8
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-22 08:41:34 -0300

    chore: update shareCode to VWUIMRXVVC7

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.bateu.bet.br/5n4o0boe?shareCode=NKPA82SKDCE&afp8=tapi100&home=1" });
+    res.writeHead(302, { Location: "https://go.aff.bateu.bet.br/5n4o0boe?shareCode=VWUIMRXVVC7&afp8=tapi100&home=1" });
     res.end();
   }
